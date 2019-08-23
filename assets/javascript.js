5909987f75d2cb1a3560864f3e5521f119a4b852
JohnCdev <johncarverp@gmail.com>
2019-08-22 22:17:08 -0400

    javascript initial work done

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,7 +1,69 @@
-var animals = [];
+var topics = ["cat", "dog", "rat", "hamster", "giraffe"];
 
+$("#button-container").on("click", ".gif-button", function () {
+    $("#gif-container").empty();
 
+    var term = $(this).text();
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=EDLqgQRQ2EF7rzhHyY9utP3mRuRMF7h6&limit=10&q=" + term;
 
-//EDLqgQRQ2EF7rzhHyY9utP3mRuRMF7h6
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (res) {
+            console.log(res);
+            var results = res.data;
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>")
+                    .addClass("gif-div mr-2 mt-2");
+                var img = $("<img>")
+                    .addClass("gif")
+                    .attr("src", results[i].images.fixed_height_still.url)
+                    .attr("data-still", results[i].images.fixed_height_still.url)
+                    .attr("data-gif", results[i].images.fixed_height.url)
+                    .attr("data-state", "still")
+                    .attr("alt", term + " img");
+                var rating = $("<p>")
+                    .text("Rating: " + results[i].rating);
+                gifDiv.append(rating);
+                gifDiv.append(img);
+                $("#gif-container").append(gifDiv);
+            }
+        });
+});
 
-document.onload();
+$("#gif-container").on("click", ".gif", function () {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-gif"));
+        $(this).attr("data-state", "gif");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
+
+$("#button-submit").on("click", function () {
+    if ($("#button-input").val().trim() === "") {
+        return false;
+    } else {
+        topics.push($("#button-input").val().trim());
+        $("#button-container").empty();
+        topics.forEach(i => {
+            var btn = $("<button>")
+                .addClass("gif-button rounded")
+                .text(i);
+            $("#button-container").append(btn);
+        })
+        $("#button-input").val("");
+    }
+});
+
+window.onload = function () {
+    topics.forEach(i => {
+        var btn = $("<button>")
+            .addClass("gif-button rounded")
+            .text(i);
+        $("#button-container").append(btn);
+    })
+};
